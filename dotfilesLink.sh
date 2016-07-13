2785dd8aab173f059bec1b5a80d8b33c031ae224
yusuke.matsuda <e9915113@gmail.com>
2016-07-13 13:08:14 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,7 +2,9 @@
 ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.vim ~/
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.tigrc ~/.tigrc
+
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
